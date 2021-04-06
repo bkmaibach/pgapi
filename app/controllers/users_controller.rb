f5ab8c777d48b6a9cef0cb7e98ce9f8c797d884c
Keith <Keith@inputlogic.ca>
2021-04-06 09:34:52 -0700

    create user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,38 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  # GET /users/1 or /users/1.json
+  def show
+  end
+
 # GET /signup
   def new
     @user = User.new
   end
 
-  # POST /articles or /articles.json
-  # def create
-  #   @article = Article.new(article_params)
-  #   @article.user = User.first
-  #   respond_to do |format|
-  #     if @article.save
-  #       format.html { redirect_to @article, notice: "Article was successfully created." }
-  #       format.json { render :show, status: :created, location: @article }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @article.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to articles_path, notice: "Signup was successful!" }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
