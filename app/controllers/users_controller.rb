9f06ca4ecc7d0fe6cf8dfcc0c2fc0aa247d4425d
Keith <Keith@inputlogic.ca>
2021-04-06 09:54:00 -0700

    add user editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  # GET /users/1/edit
+  def edit
+  end
+
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
@@ -23,6 +27,20 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  # PATCH/PUT /users/1 or /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "User account was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
 
   private
