9150fc873def58015ba1184f99d79fa94ea50215
Keith <Keith@inputlogic.ca>
2021-04-08 11:16:41 -0700

    add functionality to category index, show, and create actions, controller tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,38 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[ show edit update destroy ]
 
   def new
+    @category = Category.new
   end
 
   def index
+    @categories = Category.all
   end
 
   def show
   end
-  
+
+  def create
+    @category = Category.new(category_params)
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, notice: "Category was created" }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
