7d291fce3e63e267de4f235881bf6aebef799a50
Keith <Keith@inputlogic.ca>
2021-04-08 11:59:59 -0700

    test invalid category rejection

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -5,11 +5,22 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     get new_category_path
     assert_response :success
     assert_difference "Category.count", 1 do
-      post categories_path, params: { category: { name: "Sports "} }
+      post categories_path, params: { category: { name: "Sports"} }
       assert_response :redirect
     end
     follow_redirect!
     assert_response :success
     assert_match "Sports", response.body
+  end
+
+  test "get new category form and reject invalid category submission" do
+    get new_category_path
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: " "} }
+    end
+    assert_response :unprocessable_entity
+    assert_select "div#error_explanation", response.body
+    assert_match "Name can't be blank", response.body
   end
 end
