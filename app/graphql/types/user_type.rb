3dde9ef9cc16c5cf8074e9d11a6e1c1946a6e96b
Keith <Keith@inputlogic.ca>
2021-04-15 11:00:07 -0700

    fix mutation redfinition error by commenting then uncommenting the offending line? and properly end user type class

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,9 +3,15 @@ module Types
     field :id, ID, null: false
     field :username, String, null: true
     field :email, String, null: true
+    field :articles, [Types::ArticleType], null: true
+    field :articles_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :password_digest, String, null: true
     field :admin, Boolean, null: true
+
+    def articles_count
+      object.articles.size
+    end
   end
 end
