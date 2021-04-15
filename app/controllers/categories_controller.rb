7dfefcbda1a6d66c33d609e5340b8f847a62b837
Keith <Keith@inputlogic.ca>
2021-04-15 08:08:19 -0700

    edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,9 @@ class CategoriesController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
   def create
     @category = Category.new(category_params)
     respond_to do |format|
@@ -21,6 +24,18 @@ class CategoriesController < ApplicationController
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to @category, notice: "Category was successfully updated." }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
